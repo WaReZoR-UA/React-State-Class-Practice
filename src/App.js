91e670bed05503757db02b2a981d4101712b92ee
WaReZoR-UA <38248645+WaReZoR-UA@users.noreply.github.com>
2023-04-01 16:48:03 -0500

    build basic static layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+            counter: this.props.counter,
+        }
+	}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+	render() {
+		const { counter } = this.state;
+		return (
+            <div className="app">
+                <div className="counter">{counter}</div>
+                <div className="controls">
+                    <button>INC</button>
+                    <button>DEC</button>
+                    <button>RND</button>
+                    <button>RESET</button>
+                </div>
+            </div>
+        )
+	}
 }
 
-export default App;
+export const StateChanger = ()=> {
+	return <App counter={0} />
+}
+
+    // 1) Начальное значение счетчика должно передаваться через props
+    // 2) INC и DEC увеличивают и уменьшают счетчик соответственно на 1. Без ограничений, но можете добавить границу в -50/50. По достижению границы ничего не происходит
+    // 3) RND изменяет счетчик в случайное значение от -50 до 50. Конструкцию можете прогуглить за 20 секунд :) Не зависит от предыдущего состояния
+    // 4) RESET сбрасывает счетчик в 0 или в начальное значение из пропсов. Выберите один из вариантов
