d934a7282419741a4639741470456908f5ade39d
WaReZoR-UA <38248645+WaReZoR-UA@users.noreply.github.com>
2023-04-01 17:43:05 -0500

    task is finished

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from 'react'
+import './App.css'
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
+    constructor(props) {
+        super(props)
+        this.state = {
             counter: this.props.counter,
         }
+    }
+
+    incNum = () => {
+        this.setState((state) => ({
+            counter:
+                state.counter !== 50 ? state.counter + 1 : state.counter,
+        }))
+    }
+
+    decNum = () => {
+        this.setState((state) => ({
+            counter: state.counter !== -50 ? state.counter - 1 : state.counter,
+        }))
+	}
+	
+	rndNum = () => {
+		this.setState(state => ({
+			counter: state.counter = Math.floor(Math.random() * (50 - -50 + 1)) + -50,
+		}))
 	}
 
+	resNum = () => {
+		this.setState({
+			counter: this.props.counter
+		})
+	}
 
-
-	render() {
-		const { counter } = this.state;
-		return (
+    render() {
+        const { counter } = this.state
+        return (
             <div className="app">
                 <div className="counter">{counter}</div>
                 <div className="controls">
-                    <button>INC</button>
-                    <button>DEC</button>
-                    <button>RND</button>
-                    <button>RESET</button>
+                    <button onClick={this.incNum}>INC</button>
+                    <button onClick={this.decNum}>DEC</button>
+                    <button onClick={this.rndNum}>RND</button>
+                    <button onClick={this.resNum}>RESET</button>
                 </div>
             </div>
         )
-	}
+    }
 }
 
-export const StateChanger = ()=> {
-	return <App counter={0} />
+export const StateChanger = () => {
+    return <App counter={0} />
 }
-
-    // 1) Начальное значение счетчика должно передаваться через props
-    // 2) INC и DEC увеличивают и уменьшают счетчик соответственно на 1. Без ограничений, но можете добавить границу в -50/50. По достижению границы ничего не происходит
-    // 3) RND изменяет счетчик в случайное значение от -50 до 50. Конструкцию можете прогуглить за 20 секунд :) Не зависит от предыдущего состояния
-    // 4) RESET сбрасывает счетчик в 0 или в начальное значение из пропсов. Выберите один из вариантов
